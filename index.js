c8ca94a4bedabddf774f90f935c77c8236a1fb4d
Marek <marek.roucka2007@gmail.com>
2024-10-13 13:30:10 +0200

    Update for deployment

--- index.js
+++ index.js
@@ -63,3 +63,7 @@ function checkWin(board, x, y, symbol) {
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
